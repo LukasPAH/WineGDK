296e6d1fd2987b55fe5da5acf9194b351ba472e9
LukasPAH <lukas4460@gmail.com>
2026-09-17 19:27:50 -0500

    xgameruntime: add XGameActivation stubs

--- dlls/xgameruntime/main.c
+++ dlls/xgameruntime/main.c
@@ -246,6 +246,10 @@ HRESULT WINAPI QueryApiImpl( const GUID *runtimeClassId, REFIID interfaceId, voi
     {
         return IXGameProtocolImpl_QueryInterface( x_gameprotocol_impl, interfaceId, out );
     }
+    else if ( IsEqualGUID( runtimeClassId, &CLSID_XGameActivationImpl ) )
+    {
+        return IXGameActivationImpl_QueryInterface( x_game_activation_impl, interfaceId, out );
+    }
     else if ( IsEqualGUID( runtimeClassId, &CLSID_XGameSaveImpl ) )
     {
         return IXGameSave_QueryInterface( x_game_save_impl, interfaceId, out );
